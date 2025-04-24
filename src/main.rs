a87d253edd2b4a6c239248bf49f8f12d3abdcc78
snacj <bbmelvin@gmx.de>
2025-04-25 00:15:38 +0200

    2025-04-25 00:15 - Trying to add cool sprites as Background yippy

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,19 @@ pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("Tower Defense", 800, 600)
+    const ROWS: u32 = 12;
+    const COLS: u32 = 16;
+
+    const TILE_SIZE: u32 = 16;
+    const SPRITE_SIZE: u32 = TILE_SIZE * 3;
+
+    const WINDOW_WIDTH: u32 = SPRITE_SIZE * ROWS;
+    const WINDOW_HEIGHT: u32 = SPRITE_SIZE * COLS;
+
+    // print the window size
+    println!("Window size: {} x {}", WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    let window = video_subsystem.window("Tower Defense", WINDOW_HEIGHT, WINDOW_WIDTH)
         .position_centered()
         .build()
         .unwrap();
@@ -20,12 +32,13 @@ pub fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let tower = texture_creator.load_texture("assets/sprites/test.png")?;
     let man = texture_creator.load_texture("assets/sprites/man.png")?;
-    let background = texture_creator.load_texture("assets/sprites/background.png")?;
+    let fluss = texture_creator.load_texture("assets/sprites/fluss2.png")?;
+    let grass = texture_creator.load_texture("assets/sprites/grass.png")?;
 
-    let tower1_dest_rect = Rect::new(50, 100, 64, 64);
+    let tower1_dest_rect = Rect::new(10, 100, 64, 64);
     let mut man_dest_rect = Rect::new(300, 200, 64, 64);
+    let fluss_dest_rect = Rect::new(0, 0, WINDOW_WIDTH * 2, WINDOW_HEIGHT);
 
-    canvas.copy(&background, None, None).expect("Failed to copy tower texture");
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -57,7 +70,13 @@ pub fn main() -> Result<(), String> {
         }
 
         // The rest of the game loop goes here...
-        canvas.copy(&background, None, None).expect("Failed to copy tower texture");
+        for i in 0..COLS {
+            for j in 0..ROWS {
+                let dest_rect = Rect::new((i * 16 * 3) as i32, (j * 16 * 3) as i32, 16*3, 16*3);
+                canvas.copy(&grass, None, Some(dest_rect)).expect("Failed to copy grass texture");
+            }
+        }
+        canvas.copy(&fluss, None, Some(fluss_dest_rect)).expect("Failed to copy tower texture");
         canvas.copy(&tower, None, Some(tower1_dest_rect)).expect("Failed to copy tower texture");
         canvas.copy(&man, None, Some(man_dest_rect)).expect("Failed to copy man texture");
 
