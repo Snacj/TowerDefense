5369a8717268dcb11bde5a3c0c369ccd31ab3830
snacj <bbmelvin@gmx.de>
2025-04-25 12:37:59 +0200

    2025-04-25 12:37 - Added comments for clarity

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,52 @@ pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    // Window Size in Rows and Columns
     const ROWS: u32 = 12;
     const COLS: u32 = 16;
 
+    // Initial Sprite Size in px
     const TILE_SIZE: u32 = 16;
-    const SPRITE_SIZE: u32 = TILE_SIZE * 4;
+    // Scaled Sprite Size in px
+    const SPRITE_SIZE: u32 = TILE_SIZE * 4; // 64x64 px
 
+    // Window Size in px
     const WINDOW_WIDTH: u32 = SPRITE_SIZE * COLS;
     const WINDOW_HEIGHT: u32 = SPRITE_SIZE * ROWS;
 
-    // print the window size
-    println!("Window size: {} x {}", WINDOW_WIDTH, WINDOW_HEIGHT);
-
+    // Create a window with { Title, Width, Height }
     let window = video_subsystem.window("Tower Defense", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
 
+    // Create a canvas to draw on
     let mut canvas = window.into_canvas().build().unwrap();
 
+    // Create a texture context
     let texture_creator = canvas.texture_creator();
+
+    // Load textures
     let tower = texture_creator.load_texture("assets/sprites/test.png")?;
     let man = texture_creator.load_texture("assets/sprites/man.png")?;
     let fluss = texture_creator.load_texture("assets/sprites/fluss2.png")?;
     let grass = texture_creator.load_texture("assets/sprites/grass.png")?;
 
-    let tower1_dest_rect = Rect::new(10, 100, 64, 64);
+    // Set destination rectangles for the textures to set size and position
+    let tower1_dest_rect = Rect::new(100, 130, 64, 64);
     let mut man_dest_rect = Rect::new(300, 200, 64, 64);
     let fluss_dest_rect = Rect::new(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    // Clear the canvas and update new renders
     canvas.clear();
     canvas.present();
+
+    // Create event pump to handle events 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    // Main game loop
     'running: loop {
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -54,34 +66,46 @@ pub fn main() -> Result<(), String> {
             }
         }
 
+        // Get the current state of the keyboard
         let keyboard_state = event_pump.keyboard_state();
 
+        // Man movement
+        // Remember that the origin (0,0) is at the top left corner of the window so Y increases
+        // downwards
+        // W = Up
         if keyboard_state.is_scancode_pressed(sdl2::keyboard::Scancode::W) {
             man_dest_rect.set_y(man_dest_rect.y() - 5);
         }
+        // S = Down
         if keyboard_state.is_scancode_pressed(sdl2::keyboard::Scancode::S) {
             man_dest_rect.set_y(man_dest_rect.y() + 5);
         }
+        // A = Left
         if keyboard_state.is_scancode_pressed(sdl2::keyboard::Scancode::A) {
             man_dest_rect.set_x(man_dest_rect.x() - 5);
         }
+        // D = Right
         if keyboard_state.is_scancode_pressed(sdl2::keyboard::Scancode::D) {
             man_dest_rect.set_x(man_dest_rect.x() + 5);
         }
 
-        // The rest of the game loop goes here...
+        // Render grass tiles on every Row Col
         for i in 0..COLS {
             for j in 0..ROWS {
                 let dest_rect = Rect::new((i * SPRITE_SIZE) as i32, (j * SPRITE_SIZE) as i32, SPRITE_SIZE, SPRITE_SIZE);
                 canvas.copy(&grass, None, Some(dest_rect)).expect("Failed to copy grass texture");
             }
         }
+        // Render rest of the textures in Order
         canvas.copy(&fluss, None, Some(fluss_dest_rect)).expect("Failed to copy tower texture");
         canvas.copy(&tower, None, Some(tower1_dest_rect)).expect("Failed to copy tower texture");
         canvas.copy(&man, None, Some(man_dest_rect)).expect("Failed to copy man texture");
 
+        // Update the canvas
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+
+        // Delay to set the Framerate
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60)); // 60 FPS
     }
     Ok(())
 }
