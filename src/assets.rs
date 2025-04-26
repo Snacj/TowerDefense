d1d5c07e0377756749b0c278474322012088439b
snacj <bbmelvin@gmx.de>
2025-04-26 15:32:53 +0200

    2025-04-26 15:32 - changed Assets

--- src/assets.rs
+++ src/assets.rs
@@ -12,7 +12,7 @@ pub struct Assets<'a> {
 
 impl<'a> Assets<'a> {
     pub fn load(texture_creator: &'a TextureCreator<WindowContext>) -> Result<Self, String> {
-        let tower = texture_creator.load_texture("assets/sprites/test.png")?;
+        let tower = texture_creator.load_texture("assets/sprites/tower.png")?;
         let man = texture_creator.load_texture("assets/sprites/man.png")?;
         let fluss = texture_creator.load_texture("assets/sprites/fluss2.png")?;
         let grass = texture_creator.load_texture("assets/sprites/grass.png")?;
